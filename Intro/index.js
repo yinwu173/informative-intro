b9dfc62ed94ac51639497e3fab4447becdc38a9d
yinwu173 <yin.wu173@gmail.com>
2024-08-26 21:08:51 -0700

    added additional messages for user input, wrote function to initialize app

--- Intro/index.js
+++ Intro/index.js
@@ -4,30 +4,71 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-   {
-    type: "input",
-    message: "What is your name?",
-    name: "name"
-   },
-   {
-    type: "input",
-    message: "How was your day?",
-    name: "day"
-   },
-   {
-    type: "input",
-    message: "What is your favorite color?",
-    name: "color"
-}];
+    {
+        type: "input",
+        message: "What is the title of your Project?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "List your project description?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Create your table of contents",
+        name: "tableOfContents",
+    },
+    {
+        type: "input",
+        message: "List your project description?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Include an installation guide.",
+        name: "installation",     
+    },
+    {
+        type: "input",
+        message: "Usage?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "License?",
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "List the contributions",
+        name: "contributions",
+    },
+    {
+        type: "input",
+        message: "Tests?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "Any Questions?",
+        name: "questions",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile('ReadMe.md', fileName, (err) =>
-        err ? console.error(err) : console.log('Sucess!')
-)};
+        err ? console.error(err) : console.log('Success!')
+    )
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log("Creating ReadMe file in progress")
+    });
+}
 
 // Function call to initialize app
 init();
