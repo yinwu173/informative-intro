145409576a295d58aff878b6255f09a12fe41b73
yinwu173 <yin.wu173@gmail.com>
2024-08-26 20:23:48 -0700

    edited index js and created an array of questions for user to answer

--- Intro/index.js
+++ Intro/index.js
@@ -2,10 +2,27 @@
 const inquirer = requirer('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   {
+    type: "input",
+    message: "What is your name?",
+    name: "name"
+   },
+   {
+    type: "input",
+    message: "How was your day?",
+    name: "day"
+   },
+   {
+    type: "input",
+    message: "What is your favorite color?",
+    name: "color"
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+};
 
 // TODO: Create a function to initialize app
 function init() {}
