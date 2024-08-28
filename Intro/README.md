973591453818634cf78d87d76e2ad73f7d9c1487
yinwu173 <yin.wu173@gmail.com>
2024-08-27 20:53:13 -0700

    created render license link and license section in the generate markdown java

---
+++ Intro/README.md
@@ -0,0 +1,31 @@
+# what a day
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description of the Project
+describe day
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributions](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Username](#username)
+* [Email](#email)
+
+### Installation
+step 1 step 2
+### Usage
+first example is ...
+### License
+MIT
+### Contributions
+credits given
+### Tests
+instructions:
+### Questions
+nope
+### Github Username
+yinwu
+### Email Address
+yinwu173
--- Intro/utils/generateMarkdown.js
+++ Intro/utils/generateMarkdown.js
@@ -1,21 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license == 'None')
-    return '';
-  if (license == 'MIT')
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-  if (license == 'Mozilla')
-    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+  if (license == 'None') { return '' };
+  if (license == 'MIT') { return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' };
+  if (license == 'Mozilla') { return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)' };
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license == 'None') { return '' };
+  if (license == 'MIT') { return '(https://opensource.org/licenses/MIT)' };
+  if (license == 'Mozilla') { return '(https://opensource.org/licenses/MPL-2.0)' };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) { 
+  if(license == 'None') { return '' }
+  else {
+    return `### License
+    ${license}
+    ${renderLicenseLink(license)}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
@@ -39,7 +47,7 @@ ${response.installation}
 ### Usage
 ${response.usage}
 ### License
-${response.license}
+${renderLicenseSection(response.license)}
 ### Contributions
 ${response.contributions}
 ### Tests
