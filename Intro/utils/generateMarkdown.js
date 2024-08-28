f429b830df10c3786980c73967f5687186f0647c
yinwu173 <yin.wu173@gmail.com>
2024-08-27 20:35:47 -0700

    edited generateMarkdown.js: created render license badge function

--- Intro/utils/generateMarkdown.js
+++ Intro/utils/generateMarkdown.js
@@ -1,8 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== none)
+  if (license == 'None')
     return '';
+  if (license == 'MIT')
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  if (license == 'Mozilla')
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
 }
 
 // TODO: Create a function that returns the license link
